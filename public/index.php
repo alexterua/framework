f5748b9c6dc7a28086841bf849f2f0a9a9869057
alexterua <alexter.od.ua@gmail.com>
2019-08-03 14:15:06 +0300

    Included composer autoload, added base dir framework, added class Request

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
-$name = $_GET['name'] ?? "Guest";
+use Framework\Http\Request;
 
+chdir(dirname(__DIR__));
+require "vendor/autoload.php";
+
+session_start();
+header('Developer: Alexterua');
+
+### Initialization
+
+$request = new Request();
+
+### Action
+
+$name = $_GET['name'] ?? "Guest";
 echo "Hello, $name!";
